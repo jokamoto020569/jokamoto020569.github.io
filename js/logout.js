89e88989333fc6d7eaaf325f5e558cd03c92e32d
jokamoto020569 <j.okamoto020569@gmail.com>
2026-02-21 11:16:59 +0900

    Implement logout functionality using Firebase Auth

---
+++ js/logout.js
@@ -0,0 +1,9 @@
+import { auth } from "./firebase.js";
+import { signOut } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+window.logout = function () {
+  signOut(auth).then(() => {
+    location.href = "login.html";
+  });
+};
