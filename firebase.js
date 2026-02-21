eb3fcdfcec7594f70cfeffe63c9f71a4f291872e
jokamoto020569 <j.okamoto020569@gmail.com>
2026-02-21 11:06:02 +0900

    Add Firebase initialization and authentication setup
    FireBase設定JS作成

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCGN2eHUm2N-MLkvCTPJLzasRjDSu0zZuw",
+  authDomain: "project-j-b1dd2.firebaseapp.com",
+  projectId: "project-j-b1dd2",
+  storageBucket: "project-j-b1dd2.firebasestorage.app",
+  messagingSenderId: "1070194947377",
+  appId: "1:1070194947377:web:895fa65d0c2302999e2c10",
+  measurementId: "G-4BV5C1P5TY"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
